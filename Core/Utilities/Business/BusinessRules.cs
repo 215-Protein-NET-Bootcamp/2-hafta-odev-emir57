2f2886a713499c946e843d1ce2bbc9bb1036990d
emir57 <emir.gurbuz06@hotmail.com>
2022-07-21 19:54:30 +0300

    BusinessRules

---
+++ Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Result;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (IResult logic in logics)
+            {
+                if (logic.Success == false)
+                    return logic;
+            }
+            return null;
+        }
+    }
+}
