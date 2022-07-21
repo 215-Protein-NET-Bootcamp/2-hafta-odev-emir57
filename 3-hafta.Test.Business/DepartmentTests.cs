1febe100cd4a3f23fd8f6788fb013b1d8dbb5007
emir57 <emir.gurbuz06@hotmail.com>
2022-07-21 17:11:32 +0300

    Complete DepartmentTests

--- 3-hafta.Test.Business/DepartmentTests.cs
+++ 3-hafta.Test.Business/DepartmentTests.cs
@@ -23,12 +23,46 @@ namespace _3_hafta.Test.Business
             _dbDepartment = getDepartmentList();
 
             _mockDepartmentService.Setup(x => x.GetListAsync()).ReturnsAsync(new SuccessDataResult<List<DepartmentDto>>(_dbDepartmentDto));
-            _mockDepartmentService.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((int departmentId) => new SuccessDataResult<DepartmentDto>(getDepartmentById(departmentId));
+            _mockDepartmentService.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((int departmentId) => new SuccessDataResult<DepartmentDto>(getDepartmentById(departmentId)));
 
             _mockDepartmentService.Setup(x => x.AddAsync(It.IsAny<DepartmentDto>())).ReturnsAsync((DepartmentDto dto) => new SuccessResult());
             _mockDepartmentService.Setup(x => x.UpdateAsync(It.IsAny<int>(), It.IsAny<DepartmentDto>())).ReturnsAsync((int departmentId, DepartmentDto dto) => new SuccessResult());
             _mockDepartmentService.Setup(x => x.DeleteAsync(It.IsAny<int>())).ReturnsAsync((int departmentId) => new SuccessResult());
 
+        }
+
+        [Fact]
+        public async void Get_all_departments()
+        {
+            var result = await _mockDepartmentService.Object.GetListAsync();
+            Assert.Equal(result.Data.Count, 2);
+        }
+        [Theory]
+        [InlineData(1)]
+        public async void Get_by_id_department(int id)
+        {
+            var result = await _mockDepartmentService.Object.GetByIdAsync(id);
+            Assert.NotEqual(result.Data, null);
+        }
+        [Fact]
+        public async void Add_department()
+        {
+            var result = await _mockDepartmentService.Object.AddAsync(new DepartmentDto());
+            Assert.Equal(result.Success, true);
+        }
+        [Theory]
+        [InlineData(1)]
+        public async void Update_department(int id)
+        {
+            var result = await _mockDepartmentService.Object.UpdateAsync(id, new DepartmentDto());
+            Assert.Equal(result.Success, true);
+        }
+        [Theory]
+        [InlineData(1)]
+        public async void Delete_department(int id)
+        {
+            var result = await _mockDepartmentService.Object.DeleteAsync(id);
+            Assert.Equal(result.Success, true);
         }
 
         private List<DepartmentDto> getDepartmentDtoList()
